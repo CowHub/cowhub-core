f45ff20b553a61db9c3e91dc0868e4aab7ef5ace
Hongjiang Liu <hongjiang.liu14@imperial.ac.uk>
2016-11-25 16:28:48 +0000

    Add RabbitMQ.

--- lib/rabbit.py
+++ lib/rabbit.py
@@ -1,22 +1,37 @@
 import pika
 import os
+import json
+
+from .worker import Worker
 
 HOST = os.environ['RABBIT_HOST']
 QUEUE = 'task_queue'
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST))
-channel = connection.channel()
-
-channel.queue_declare(queue=QUEUE, durable=True)
-print(' [*] Waiting for messages. To exit press CTRL+C')
+WORKER = Worker()
 
 
-def callback(ch, method, properties, body):
-    # TODO: Add consumer method
+def callback(ch, method, props, body):
+    body_data = json.loads(body)
+    mode = body_data['mode']
+
+    if mode == 'register':
+        params = tuple(body_data[x] for x in ['cattle_id', 'image'])
+        WORKER.start_processing(*params)
+    elif mode == 'match':
+        params = tuple(body_data[x] for x in ['image', 'server_id', 'request_id'])
+        WORKER.start_matching(*params)
+
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(callback, queue=QUEUE)
+if __name__ == '__main__':
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST))
+    channel = connection.channel()
 
-channel.start_consuming()
+    channel.queue_declare(queue=QUEUE, durable=True)
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(callback, queue=QUEUE)
+
+    channel.start_consuming()
--- lib/worker.py
+++ lib/worker.py
@@ -12,33 +12,17 @@ from .db import DbConn
 from .util import foreach
 
 
-def read_base64(base64_string):
+def read_base64(b64_string):
     sbuf = StringIO()
-    sbuf.write(base64.b64decode(base64_string))
+    sbuf.write(base64.b64decode(b64_string))
     pimg = Image.open(sbuf)
     return cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
 
 
-def start_matching(image, server_id, request_id):
-    image = read_base64(image)
+def base64_to_descriptor(b64_string):
+    image = read_base64(b64_string)
     (_, edge, _) = crop(image)
-    descriptor = get_descriptor(edge)
-
-    db_conn = DbConn()
-    processes = cpu_count()
-    iter_queue = Queue(1)
-    res_queue = Queue(1)
-
-    workers = [Process(target=start_worker, args=(descriptor, db_conn, iter_queue, res_queue))
-               for _ in range(processes)]
-    foreach(lambda x: x.start(), workers)
-    foreach(lambda x: x.join(), workers)
-
-    try:
-        (cid, diff) = res_queue.get(block=False)
-        db_conn.publish(cid, diff, server_id, request_id)
-    except QueueEmpty:
-        raise RuntimeError('No element in the result queue.')
+    return get_descriptor(edge)
 
 
 def start_worker(source_descriptor, redis_conn, iter_queue, res_queue):
@@ -58,3 +42,32 @@ def start_worker(source_descriptor, redis_conn, iter_queue, res_queue):
 
             (other_cid, other_diff) = res_queue.get()
             res_queue.put((cid, diff) if diff < other_diff else (other_cid, other_diff))
+
+
+class Worker:
+    def __init__(self):
+        self._db_conn = DbConn()
+
+    def start_processing(self, image, cid):
+        descriptor = base64_to_descriptor(image)
+
+        self._db_conn.write(cid, descriptor)
+
+    def start_matching(self, image, server_id, request_id):
+        descriptor = base64_to_descriptor(image)
+
+        db_conn = self._db_conn
+        processes = cpu_count()
+        iter_queue = Queue(1)
+        res_queue = Queue(1)
+
+        workers = [Process(target=start_worker, args=(descriptor, db_conn, iter_queue, res_queue))
+                   for _ in range(processes)]
+        foreach(lambda x: x.start(), workers)
+        foreach(lambda x: x.join(), workers)
+
+        try:
+            (cid, diff) = res_queue.get(block=False)
+            db_conn.publish(cid, diff, server_id, request_id)
+        except QueueEmpty:
+            raise RuntimeError('No element in the result queue.')
