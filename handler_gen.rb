519829645c155735b0957a5950f5a78f35ce0a9e
Hongjiang Liu <hongjiang.liu14@imperial.ac.uk>
2016-12-02 17:41:28 +0000

    Use script to generate zip for lambda

--- handler_gen.rb
+++ handler_gen.rb
@@ -1,1 +1,11 @@
 #!/usr/bin/env ruby
+require 'fileutils'
+
+abort 'Invalid entry point of arguments.' if __FILE__ != $0 or ARGV.length == 0
+
+mode = ARGV.first
+
+FileUtils::cp "lib/#{mode}/handler.py", 'lib/'
+system 'zip -r lib{.zip,}'
+FileUtils::rm 'lib/handler.py'
+
