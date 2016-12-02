ea4354c7e5c3278bd92e84dcf015b027eee9ba5e
Hongjiang Liu <hongjiang.liu14@imperial.ac.uk>
2016-12-02 19:15:11 +0000

    Revert to base64 image loading.

--- lib/worker.py
+++ lib/worker.py
@@ -4,20 +4,21 @@ from StringIO import StringIO
 import sys
 from PIL import Image
 import numpy as np
+import base64
 
 from descriptor import get_descriptor
 from edging import get_edge
 
 
-def read_blob(blob):
+def read_base64(blob):
     sbuf = StringIO()
-    sbuf.write(blob)
+    sbuf.write(base64.b64decode(blob))
     pimg = Image.open(sbuf)
     return cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
 
 
 def generate_descriptor(blob):
-    image = read_blob(blob)
+    image = read_base64(blob)
     _, edge = get_edge(image)
     return get_descriptor(edge)
 
@@ -41,7 +42,7 @@ def find_match(blob, potential_matches):
 
 
 if __name__ == "__main__":
-    image = read_blob(sys.argv[1])
+    image = read_base64(sys.argv[1])
     descriptor = generate_descriptor(image)
     encoded_descriptor = pickle.dumps(descriptor)
     print(encoded_descriptor)
