d461c36ce2a01d213db3aa0ffa6089730df6ee8a
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-01-07 22:27:20 +0000

    Use weave independently

--- lib/thinning.py
+++ lib/thinning.py
@@ -1,57 +1,49 @@
-"""
-===========================
-@Author  : Linbo<linbo.me>
-@Version: 1.0    25/10/2014
-This is the implementation of the
-Zhang-Suen Thinning Algorithm for skeletonization.
-===========================
-"""
+import weave
+import numpy as np
 
 
-def neighbours(x, y, image):
-    """Return 8-neighbours of image point P1(x,y), in a clockwise order"""
-    img = image
-    x_1, y_1, x1, y1 = x - 1, y - 1, x + 1, y + 1
-    return [img[x_1][y], img[x_1][y1], img[x][y1], img[x1][y1],  # P2,P3,P4,P5
-            img[x1][y], img[x1][y_1], img[x][y_1], img[x_1][y_1]]  # P6,P7,P8,P9
+def _thinningIteration(im, iter):
+    I, M = im, np.zeros(im.shape, np.uint8)
+    expr = """
+	for (int i = 1; i < NI[0]-1; i++) {
+		for (int j = 1; j < NI[1]-1; j++) {
+			int p2 = I2(i-1, j);
+			int p3 = I2(i-1, j+1);
+			int p4 = I2(i, j+1);
+			int p5 = I2(i+1, j+1);
+			int p6 = I2(i+1, j);
+			int p7 = I2(i+1, j-1);
+			int p8 = I2(i, j-1);
+			int p9 = I2(i-1, j-1);
+			int A  = (p2 == 0 && p3 == 1) + (p3 == 0 && p4 == 1) +
+			         (p4 == 0 && p5 == 1) + (p5 == 0 && p6 == 1) +
+			         (p6 == 0 && p7 == 1) + (p7 == 0 && p8 == 1) +
+			         (p8 == 0 && p9 == 1) + (p9 == 0 && p2 == 1);
+			int B  = p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9;
+			int m1 = iter == 0 ? (p2 * p4 * p6) : (p2 * p4 * p8);
+			int m2 = iter == 0 ? (p4 * p6 * p8) : (p2 * p6 * p8);
+			if (A == 1 && B >= 2 && B <= 6 && m1 == 0 && m2 == 0) {
+				M2(i,j) = 1;
+			}
+		}
+	}
+	"""
+
+    weave.inline(expr, ["I", "iter", "M"])
+    return (I & ~M)
 
 
-def transitions(neighbours):
-    """No. of 0,1 patterns (transitions from 0 to 1) in the ordered sequence"""
-    n = neighbours + neighbours[0:1]  # P2, P3, ... , P8, P9, P2
-    return sum((n1, n2) == (0, 255) for n1, n2 in zip(n, n[1:]))  # (P2,P3), (P3,P4), ... , (P8,P9), (P9,P2)
+def thinning(src):
+    dst = src.copy() / 255
+    prev = np.zeros(src.shape[:2], np.uint8)
+    diff = None
 
+    while True:
+        dst = _thinningIteration(dst, 0)
+        dst = _thinningIteration(dst, 1)
+        diff = np.absolute(dst - prev)
+        prev = dst.copy()
+        if np.sum(diff) == 0:
+            break
 
-def thinning(image):
-    """the Zhang-Suen Thinning Algorithm"""
-    image_thinned = image.copy()  # deepcopy to protect the original image
-    changing1 = changing2 = 1  # the points to be removed (set as 0)
-    while changing1 or changing2:  # iterates until no further changes occur in the image
-        # Step 1
-        changing1 = []
-        rows, columns = image_thinned.shape  # x for rows, y for columns
-        for x in range(1, rows - 1):  # No. of  rows
-            for y in range(1, columns - 1):  # No. of columns
-                P2, P3, P4, P5, P6, P7, P8, P9 = n = neighbours(x, y, image_thinned)
-                if (image_thinned[x][y] == 255 and
-                    510 <= sum(n) <= 1530 and  # Condition 1: 2<= N(P1) <= 6
-                    transitions(n) == 1 and  # Condition 2: S(P1)=1
-                    any(p == 0 for p in (P2, P4, P6)) and  # Condition 3
-                    any(p == 0 for p in (P4, P6, P8))):  # Condition 4
-                    changing1.append((x, y))
-        for x, y in changing1:
-            image_thinned[x][y] = 0
-        # Step 2
-        changing2 = []
-        for x in range(1, rows - 1):
-            for y in range(1, columns - 1):
-                P2, P3, P4, P5, P6, P7, P8, P9 = n = neighbours(x, y, image_thinned)
-                if (image_thinned[x][y] == 255 and  # Condition 0
-                    510 <= sum(n) <= 1530 and  # Condition 1
-                    transitions(n) == 1 and  # Condition 2
-                    any(p == 0 for p in (P2, P4, P8))and  # Condition 3
-                    any(p == 0 for p in (P2, P6, P8))):  # Condition 4
-                    changing2.append((x, y))
-        for x, y in changing2:
-            image_thinned[x][y] = 0
-    return image_thinned
+    return dst * 255
