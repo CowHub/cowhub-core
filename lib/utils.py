c0429780b63eb6edac93cf7fd42827e78fd63fcd
Hongjiang Liu <hongjiang.liu14@imperial.ac.uk>
2017-01-08 00:47:03 +0000

    I love pickled onions

---
+++ lib/utils.py
@@ -0,0 +1,19 @@
+import cPickle as pickle
+import numpy
+import cv2
+
+
+def kp_dumps(kps):
+    index = numpy.empty(len(kps), dtype=tuple)
+    for (i, kp) in enumerate(kps):
+        index[i] = (kp.pt, kp.size, kp.angle, kp.response, kp.octave, kp.class_id)
+    return pickle.dumps(index)
+
+
+def kp_loads(string):
+    index = pickle.loads(string)
+    array = numpy.empty(index.shape, dtype=object)
+    for i in range(0, index.shape):
+        t = index[i]
+        array[i] = cv2.KeyPoint(t[0][0], t[0][1], _size=t[1], _angle=t[2], _response=t[3], _octave=t[4], _class_id=t[5])
+    return array.tolist()
