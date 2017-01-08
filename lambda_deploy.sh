a5e2cd586679c085ef10f925fdbb152eb8467a3a
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-01-08 02:44:25 +0000

    Include console messages

--- lambda_deploy.sh
+++ lambda_deploy.sh
@@ -8,15 +8,25 @@ S3_KEY_REGISTER="cowhub-image-register/$RANDOM-$RANDOM-$RANDOM"
 
 echo "Deploying $FUNCTION"
 
+echo "BUILDING DOCKER IMAGE"
+
 ./build_docker.sh || exit 1
 
+echo "REMOVING PREVIOUS RESULTS"
+
 make clean || exit 5
+
+echo "RUNNING DOCKER IMAGE"
 
 docker run -itd -v "$PWD/package-lib-$FUNCTION":/package-lib \
            cowhub/cowhub-core \
            /bin/bash -c 'cp /stack.tgz /package-lib/' || exit 2
 
+echo "MAKING AND VERIFYING PACKAGE"
+
 ./make_and_verify_package.sh $FUNCTION || exit 3
+
+echo "COPYING TO AWS"
 
 aws s3 cp "$FUNCTION.zip" "s3://$S3_BUCKET/$S3_KEY_REGISTER" || exit 4
 aws lambda update-function-code \
